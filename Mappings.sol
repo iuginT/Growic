f0b826ef0fd7d1eaa16ea0304123f1de38a5a80e
Eugen Toma <96338896+iuginT@users.noreply.github.com>
2022-12-28 15:17:19 +0200

    Update Mappings.sol

--- Mappings.sol
+++ Mappings.sol
@@ -1,1 +1,18 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
 
+contract SecondTask {
+
+    mapping(address => uint) balance;
+
+    function deposit(uint256 amount) external {
+
+        balance[msg.sender] += amount;
+
+    } 
+
+    function checkBalance() external view returns(uint _balance) {
+        _balance = balance[msg.sender];
+    }
+
+}
