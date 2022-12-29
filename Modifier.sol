4280907c582206899677a3ac1cf8ac318b5ebfa2
Eugen Toma <96338896+iuginT@users.noreply.github.com>
2022-12-29 14:46:39 +0200

    Create Modifier.sol

---
+++ Modifier.sol
@@ -0,0 +1,71 @@
+
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract SecondTask {
+
+    struct User {
+        string name;
+        uint8 age;
+    }
+
+    error AmountToSmall();
+
+    address payable owner;
+    uint256 private Fee;
+
+    mapping(address => uint) balance;
+    mapping(address => User) users;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "not owner");
+        _;
+    }
+
+    modifier alreadyDeposit() {
+        require(balance[msg.sender] > 0);
+        _;
+    }
+
+    modifier value(uint256 _amount) {
+        if(_amount < Fee){
+            revert AmountToSmall();
+            
+        }
+        _;
+    }
+
+    constructor(uint256 _fee) {
+        owner = payable(msg.sender); // not sure about the owner so I made sure to set him here
+        Fee = _fee;
+    }
+
+    function setUserDetails(string calldata name, uint8 age) external {
+        users[msg.sender] = (User(name, age));
+    }
+
+    function getUserDetail() public view returns(User memory){
+        return users[msg.sender];
+
+    }
+
+    function deposit(uint256 amount) external {
+
+        balance[msg.sender] += amount;
+
+    } 
+
+    function withdraw() external onlyOwner {
+        (bool success, ) = payable(owner).call{value: address(this).balance}('');
+        require(success);
+    }
+
+    function addFund(uint256 _amount) external alreadyDeposit value(_amount) {
+            balance[msg.sender] += _amount;
+    }
+
+    function checkBalance() external view returns(uint _balance) {
+        _balance = balance[msg.sender];
+    }
+
+}
